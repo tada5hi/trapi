ed7646acfd819717f8ec008d30e31625eea96883
tada5hi <peter.placzek1996@gmail.com>
2022-01-22 15:24:51 +0100

    updated dependencies + cleared error constants

--- packages/trapi-client/src/error/constants.ts
+++ packages/trapi-client/src/error/constants.ts
@@ -8,10 +8,5 @@
 export enum ErrorCode {
     AUTHORIZATION_HEADER_INVALID = 'auth_header_invalid',
     AUTHORIZATION_HEADER_PARSE = 'auth_header_parse',
-    AUTHORIZATION_HEADER_TYPE_PARSE = 'auth_header_Type_parse',
-    AUTH_HEADER_TYPE_UNSUPPORTED = 'auth_header_type_unsupported',
-    CREDENTIALS_INVALID = 'invalid_credentials',
-    TOKEN_INVALID = 'invalid_token',
-    TOKEN_EXPIRED = 'invalid_token',
-    TOKEN_SUB_KIND_INVALID = 'token_sub_kind_invalid',
+    AUTHORIZATION_HEADER_TYPE_PARSE = 'auth_header_type_parse',
 }

049eaaa9b98f8175fe745e6a6346503bbcabca8d
tada5hi <peter.placzek1996@gmail.com>
2022-04-28 15:32:18 +0200

    add js-doc to trapi client module

--- packages/client/src/module.ts
+++ packages/client/src/module.ts
@@ -26,96 +26,211 @@ export class Client {
 
     // ---------------------------------------------------------------------------------
 
+    /**
+     * Return driver config.
+     *
+     * @return AxiosDefaults<any>
+     */
     get config() : AxiosDefaults<any> {
         return this.driver.defaults;
     }
 
+    /**
+     * Return base url
+     *
+     * @param config
+     *
+     * @return string
+     */
     public getUri(config?: ClientRequestConfig): string {
         return this.driver.getUri(config);
     }
 
     // ---------------------------------------------------------------------------------
 
+    /**
+     * Set a header for all upcoming requests.
+     *
+     * @param key
+     * @param value
+     */
     public setHeader(key: string, value: string) {
         this.driver.defaults.headers.common[key] = value;
+
+        return this;
     }
 
+    /**
+     * Unset a specific for all upcoming requests.
+     *
+     * @param key
+     */
     public unsetHeader(key: string) {
         if (key in this.driver.defaults.headers.common) {
             delete this.driver.defaults.headers.common[key];
         }
+
+        return this;
     }
 
+    /**
+     * Unset all defined headers for the upcoming requests.
+     */
     public resetHeader() {
         this.driver.defaults.headers.common = {};
+
+        return this;
     }
 
     // ---------------------------------------------------------------------------------
 
+    /**
+     * Set an authorization header (basic, api-key, bearer).
+     *
+     * @param options
+     */
     public setAuthorizationHeader(options: AuthorizationHeader) {
         this.setHeader('Authorization', stringifyAuthorizationHeader(options));
+
+        return this;
     }
 
+    /**
+     * Unset an authorization header.
+     */
     public unsetAuthorizationHeader() {
         this.unsetHeader('Authorization');
+
+        return this;
     }
 
     // ---------------------------------------------------------------------------------
 
+    /**
+     * Make a custom request.
+     *
+     * @param config
+     */
     public request<T, R = ClientResponse<T>>(config: ClientRequestConfig): Promise<R> {
         return this.driver.request(config);
     }
 
     // ---------------------------------------------------------------------------------
 
+    /**
+     * Request a resource with the GET method.
+     *
+     * @param url
+     * @param config
+     */
     public get<T, R = ClientResponse<T>>(url: string, config?: ClientRequestConfig): Promise<R> {
         return this.driver.get(url, config);
     }
 
     // ---------------------------------------------------------------------------------
 
+    /**
+     * Delete a resource with the DELETE method.
+     *
+     * @param url
+     * @param config
+     */
     public delete<T, R = ClientResponse<T>>(url: string, config?: ClientRequestConfig): Promise<R> {
         return this.driver.delete(url, config);
     }
 
     // ---------------------------------------------------------------------------------
 
+    /**
+     * Make a verification resource with the HEAD method.
+     *
+     * @param url
+     * @param config
+     */
     public head<T, R = ClientResponse<T>>(url: string, config?: ClientRequestConfig): Promise<R> {
         return this.driver.head(url, config);
     }
 
     // ---------------------------------------------------------------------------------
 
+    /**
+     * Create a resource with the POST method.
+     *
+     * @param url
+     * @param data
+     * @param config
+     */
     public post<T, R = ClientResponse<T>>(url: string, data?: any, config?: ClientRequestConfig): Promise<R> {
         return this.driver.post(url, data, config);
     }
 
+    /**
+     * Create a resource with the POST method and FormData formatted payload.
+     *
+     * @param url
+     * @param data
+     * @param config
+     */
     public postForm<T, R = ClientResponse<T>>(url: string, data?: any, config?: ClientRequestConfig): Promise<R> {
         return this.driver.postForm(url, data, config);
     }
 
     // ---------------------------------------------------------------------------------
 
+    /**
+     * Update a resource with the PUT method.
+     *
+     * @param url
+     * @param data
+     * @param config
+     */
     public put<T, R = ClientResponse<T>>(url: string, data?: any, config?: ClientRequestConfig): Promise<R> {
         return this.driver.put(url, data, config);
     }
 
+    /**
+     * Update a resource with the PUT method and FormData formatted payload.
+     *
+     * @param url
+     * @param data
+     * @param config
+     */
     public putForm<T, R = ClientResponse<T>>(url: string, data?: any, config?: ClientRequestConfig): Promise<R> {
         return this.driver.putForm(url, data, config);
     }
 
     // ---------------------------------------------------------------------------------
 
+    /**
+     * Update a resource with the PATCH method.
+     *
+     * @param url
+     * @param data
+     * @param config
+     */
     public patch<T, R = ClientResponse<T>>(url: string, data?: any, config?: ClientRequestConfig): Promise<R> {
         return this.driver.patch(url, data, config);
     }
 
+    /**
+     * Update a resource with the PATCH method and FormData formatted payload.
+     *
+     * @param url
+     * @param data
+     * @param config
+     */
     public patchForm<T, R = ClientResponse<T>>(url: string, data?: any, config?: ClientRequestConfig): Promise<R> {
         return this.driver.patchForm(url, data, config);
     }
 
     //---------------------------------------------------------------------------------
 
+    /**
+     * Mount a response interceptor.
+     *
+     * @param onFulfilled
+     * @param onRejected
+     */
     public mountResponseInterceptor(
         onFulfilled: (value: ClientResponse<any>) => any | Promise<ClientResponse<any>>,
         onRejected: (error: any) => any,
@@ -123,12 +238,25 @@ export class Client {
         return this.driver.interceptors.response.use(onFulfilled, onRejected);
     }
 
+    /**
+     * Unmount a response interceptor.
+     *
+     * @param id
+     */
     public unmountResponseInterceptor(id: number) {
         this.driver.interceptors.response.eject(id);
+
+        return this;
     }
 
     //---------------------------------------------------------------------------------
 
+    /**
+     * Mount a request interceptor.
+     *
+     * @param onFulfilled
+     * @param onRejected
+     */
     public mountRequestInterceptor(
         onFulfilled: (value: ClientRequestConfig) => any | Promise<ClientRequestConfig>,
         onRejected: (error: any) => any,
@@ -136,7 +264,14 @@ export class Client {
         return this.driver.interceptors.request.use(onFulfilled, onRejected);
     }
 
+    /**
+     * Unmount a request interceptor.
+     *
+     * @param id
+     */
     public unmountRequestInterceptor(id: number) {
         this.driver.interceptors.request.eject(id);
+
+        return this;
     }
 }

abb8bb5f4e056a34c64ba65a63e1fae19b3d1603
tada5hi <tada5hi@users.noreply.github.com>
2023-05-15 11:40:50 +0000

    deploy: fa8835779aee968da97351324d12978b6acca635

--- assets/guide_index.md.4f583f1e.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as a,o as e,N as o}from"./chunks/framework.0799945b.js";const m=JSON.parse('{"title":"What is it?","description":"","frontmatter":{},"headers":[],"relativePath":"guide/index.md"}'),r={name:"guide/index.md"},i=o("",6),n=[i];function s(c,l,d,g,p,h){return e(),a("div",null,n)}const _=t(r,[["render",s]]);export{m as __pageData,_ as default};

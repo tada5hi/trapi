102255d505f478f5c44e31efd61b70929ade08f0
tada5hi <tada5hi@users.noreply.github.com>
2026-07-20 20:38:35 +0000

    deploy: 1897fdc26c3abfac2034bbebc924bcb7ae11322c

---
+++ assets/guide_cli.md.DEiPb8aO.lean.js
@@ -0,0 +1,1 @@
+import{_ as i,o as a,c as e,ae as t}from"./chunks/framework.D9_ZvfG7.js";const k=JSON.parse('{"title":"CLI","description":"","frontmatter":{},"headers":[],"relativePath":"guide/cli.md","filePath":"guide/cli.md"}'),n={name:"guide/cli.md"};function p(l,s,h,r,d,o){return a(),e("div",null,[...s[0]||(s[0]=[t("",58)])])}const g=i(n,[["render",p]]);export{k as __pageData,g as default};
